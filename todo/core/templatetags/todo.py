d25a432776032397f51aa2baaeb365296d1f64d8
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-07-15 15:57:18 +0900

    Show colored priority

--- todo/core/templatetags/todo.py
+++ todo/core/templatetags/todo.py
@@ -1,15 +1,43 @@
 from django import template
-from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+
 from todo.core import models
+
 register = template.Library()
+
 
 @register.filter
 def icon(task):
     if task.repeating:
-        return 'R'
+        return "R"
     if task.status == models.Task.STATUS_OPEN:
-        return '[ ]'
+        return "[ ]"
     if task.status == models.Task.STATUS_CANCELED:
-        return '[X]'
+        return "[X]"
     if task.status == models.Task.STATUS_CLOSED:
-        return '[/]'
+        return "[/]"
+
+
+@register.filter(needs_autoescape=False, is_safe=True)
+def priority(task):
+    if task.priority < 1:
+        return mark_safe(
+            '<span class="badge badge-pill badge-secondary">{}</span>'.format(
+                task.priority
+            )
+        )
+    if task.priority < 5:
+        return mark_safe(
+            '<span class="badge badge-pill badge-succes">{}</span>'.format(
+                task.priority
+            )
+        )
+    if task.priority < 7:
+        return mark_safe(
+            '<span class="badge badge-pill badge-warning">{}</span>'.format(
+                task.priority
+            )
+        )
+    return mark_safe(
+        '<span class="badge badge-pill badge-danger">{}</span>'.format(task.priority)
+    )

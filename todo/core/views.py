14f3e2e31795b6a3c509b70a70e00bbc32b9212b
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-07-15 15:42:50 +0900

    Test sorting for Today view

--- todo/core/views.py
+++ todo/core/views.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.base import TemplateView, View
 from django.views.generic.list import ListView
@@ -25,44 +26,55 @@ class Inbox(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return self.model.objects.filter(
             owner=self.request.user, project=None, status=models.Task.STATUS_OPEN
-        ).order_by('due')
+        ).order_by("due")
 
 
-class Project(LoginRequiredMixin, ListView):
+class Today(Inbox):
+    def get_queryset(self):
+        today = datetime.date.today()
+        return (
+            self.model.objects.filter(owner=self.request.user)
+            .filter(Q(start__lte=today) | Q(start__lte=today) & Q(due__lte=today))
+            .order_by("due", "start")
+        )
 
-    model = models.Task
 
-    def get_context_data(self, **kwargs):
-        context = super(Project, self).get_context_data(**kwargs)
-        context["project_list"] = models.Project.objects.filter(owner=self.request.user)
-        return context
+class Upcoming(Inbox):
+    def get_queryset(self):
+        start = datetime.date.today()
+        end = start + datetime.timedelta(days=7)
+        return self.model.objects.filter(
+            owner=self.request.user, due__gt=start, due__lte=end
+        ).order_by("due", "start")
 
+
+class Project(Inbox):
     def get_queryset(self):
         return self.model.objects.filter(
             owner=self.request.user,
             project=self.kwargs["uuid"],
-            status=self.request.GET.get('status', models.Task.STATUS_OPEN),
-        ).order_by('due')
+            status=self.request.GET.get("status", models.Task.STATUS_OPEN),
+        ).order_by("due", "start")
 
 
 class Task(LoginRequiredMixin, View):
     def ts(self, value):
-        if value == 'today':
+        if value == "today":
             return datetime.date.today()
-        if value == 'yesterday':
+        if value == "yesterday":
             return datetime.date.today() - datetime.timedelta(days=1)
-        if value == 'tomorrow':
+        if value == "tomorrow":
             return datetime.date.today() + datetime.timedelta(days=1)
         return None
 
     def post(self, request, uuid):
         task = get_object_or_404(models.Task, uuid=uuid)
-        if 'start' in self.request.POST:
-            task.start = self.ts(self.request.POST['start'])
+        if "start" in self.request.POST:
+            task.start = self.ts(self.request.POST["start"])
             task.save()
-        if 'due' in self.request.POST:
-            task.due = self.ts(self.request.POST['due'])
+        if "due" in self.request.POST:
+            task.due = self.ts(self.request.POST["due"])
             task.save()
 
         print(self.request.POST)
-        return redirect('project', task.project.uuid)
+        return redirect("project", task.project.uuid)
--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
 from todo.core import views
 
 urlpatterns = [
     path("", views.About.as_view(), name="about"),
+    path("today", views.Today.as_view(), name="today"),
+    path("upcoming", views.Upcoming.as_view(), name="upcoming"),
     path("project/", views.Inbox.as_view(), name="inbox"),
     path("project/<uuid>", views.Project.as_view(), name="project"),
     path("task/<uuid>", views.Task.as_view(), name="task"),
@@ -14,4 +17,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))

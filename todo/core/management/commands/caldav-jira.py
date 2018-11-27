7e1b75cb55abc2236564c5835880e7112b9f87ca
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-11-27 21:25:55 +0900

    Refactoring to prepare for two way sync based on caldav-github command * Refactor code into 'new_issue' * Stub for future 'old_issue' and 'push_jira' methods

--- todo/core/management/commands/caldav-jira.py
+++ todo/core/management/commands/caldav-jira.py
@@ -1,3 +1,4 @@
+import collections
 import configparser
 import logging
 import os
@@ -8,20 +9,63 @@ from dateutil.parser import parse
 from django.core.management.base import BaseCommand
 from todo import CONFIG_DIR
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     help = "Import projects from JIRA"
 
-    def handle(self, **options):
+    def handle(self, verbosity, **options):
+        if verbosity > 1:
+            logging.root.setLevel(logging.INFO)
+        if verbosity > 2:
+            logging.root.setLevel(logging.DEBUG)
+
         config = configparser.ConfigParser()
         with open(os.path.join(CONFIG_DIR, "todo-sync.ini")) as fp:
             config.readfp(fp)
 
-        caldav = ("jira", config["DEFAULTS"]["password"])
+        caldav = (config["DEFAULTS"]["user"], config["DEFAULTS"]["password"])
 
+        for project, todo in self.process(config):
+            cal = icalendar.Calendar()
+            cal["version"] = "2.0"
+            cal["prodid"] = "test"
+            cal.add_component(todo)
+
+            if verbosity > 3:
+                print(cal.to_ical().decode("utf8"))
+
+            result = requests.put(
+                "{}/{}.ics".format(config[project]["calendar"], todo["uid"]),
+                auth=caldav,
+                data=cal.to_ical(),
+            )
+            # print(result.text)
+            result.raise_for_status()
+            # return
+
+    def fetch_caldav(self, config, projects):
+        new_issue = collections.defaultdict(list)
+        old_issue = collections.defaultdict(dict)
+
+        for section in projects:
+            caldav = (config["DEFAULTS"]["user"], config["DEFAULTS"]["password"])
+            cal_request = requests.get(config[section]["calendar"], auth=caldav)
+            cal_request.raise_for_status()
+            calendar = icalendar.Calendar.from_ical(cal_request.text)
+
+            for i in calendar.walk("vtodo"):
+                if "X-JIRA-ISSUE" in i:
+                    old_issue[section][
+                        int(i.decoded("X-JIRA-ISSUE").decode("utf8"))
+                    ] = i
+                else:
+                    new_issue[section].append(i)
+        return new_issue, old_issue
+
+    def fetch_jira(self, config):
         query = "updated >= -30d AND assignee in (currentUser())"
 
         url = "{}/rest/api/latest/search".format(os.environ.get("JIRA_URL"))
@@ -46,52 +90,62 @@ class Command(BaseCommand):
         response.raise_for_status()
 
         for issue in response.json().get("issues", {}):
-            cal = icalendar.Calendar()
-            cal["version"] = "2.0"
-            cal["prodid"] = "test"
+            yield issue
 
-            event = icalendar.Todo()
-            event["uid"] = issue["id"]
-            event["summary"] = "{} {}".format(issue["key"], issue["fields"]["summary"])
-            event["description"] = "{}\n{}".format(
-                "{}/browse/{}".format(os.environ.get("JIRA_URL"), issue["key"]),
-                issue["fields"]["description"],
-            )
+    def process(self, config):
+        projects = [s for s in config._sections if s != "DEFAULTS"]
+        # Fetch our old issues so that we can compare them
+        new_caldav, old_caldav = self.fetch_caldav(config, projects)
 
-            event.add("dtstart", parse(issue["fields"]["created"]))
+        # Loop through our JIRA issues
+        for issue in self.fetch_jira(config):
+            project = issue["fields"]["project"]["key"]
+            issue_id = int(issue["id"])
 
-            if issue["fields"]["resolutiondate"]:
-                event.add("completed", parse(issue["fields"]["resolutiondate"]))
-                event.add("status", "completed")
-            if issue["fields"]["duedate"]:
-                event.add("due", parse(issue["fields"]["duedate"]))
+            if issue_id in old_caldav[project]:
+                yield project, self.old_issue(issue, old_caldav[project][issue_id])
+            else:
+                yield project, self.new_issue(issue)
 
-                alarm = icalendar.Alarm()
-                alarm["uid"] = issue["id"] + "-alarm"
-                alarm["action"] = "display"
-                alarm.add("trigger", parse(issue["fields"]["duedate"]))
-                event.add_component(alarm)
+        for project in new_caldav:
+            for vObject in new_caldav[project]:
+                yield project, self.push_jira(vObject)
 
-            section = issue["fields"]["project"]["key"]
-            if section not in config:
-                print("skipping", issue["fields"]["project"]["key"])
-                continue
-            # "status": state(issue["fields"]["status"]),
+    def new_issue(self, issue):
+        logger.info("New JIRA->CalDav #%s %s", issue["key"], issue["fields"]["summary"])
+        event = icalendar.Todo()
+        event["uid"] = issue["id"]
+        event["summary"] = "{} {}".format(issue["key"], issue["fields"]["summary"])
+        event["description"] = "{}\n{}".format(
+            "{}/browse/{}".format(os.environ.get("JIRA_URL"), issue["key"]),
+            issue["fields"]["description"],
+        )
 
-            cal.add_component(event)
+        event.add("dtstart", parse(issue["fields"]["created"]))
 
-            # print(cal.to_ical().decode("utf8"))
-            # pprint(issue)
+        if issue["fields"]["resolutiondate"]:
+            event.add("completed", parse(issue["fields"]["resolutiondate"]))
+            event.add("status", "completed")
+        if issue["fields"]["duedate"]:
+            event.add("due", parse(issue["fields"]["duedate"]))
 
-            result = requests.put(
-                "{}/jira/{}/{}.ics".format(
-                    config["DEFAULTS"]["server"],
-                    config[section]["calendar"].upper(),
-                    event["uid"],
-                ),
-                auth=caldav,
-                data=cal.to_ical(),
-            )
-            # print(result.text)
-            result.raise_for_status()
-            # return
+            alarm = icalendar.Alarm()
+            alarm["uid"] = issue["id"] + "-alarm"
+            alarm["action"] = "display"
+            alarm.add("trigger", parse(issue["fields"]["duedate"]))
+            event.add_component(alarm)
+
+        event.add("X-JIRA-ISSUE", issue["id"])
+        event.add("X-JIRA-KEY", issue["key"])
+        event.add("X-JIRA-PROJECT", issue["fields"]["project"]["key"])
+
+        return event
+
+    def old_issue(self, issue, vObject):
+        logger.info("Old JIRA->CalDav #%s %s", issue["key"], issue["fields"]["summary"])
+        # TODO: Update object
+        return vObject
+
+    def push_jira(self, vTodo):
+        logger.warn("New CalDav->JIRA %s", vTodo.decoded("summary").decode("utf8"))
+        return vTodo

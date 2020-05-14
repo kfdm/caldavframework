f034c6e3bc61571eaed01fe5a5a74cec12249976
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-05-14 18:36:40 +0900

    Some formatting

--- todo/standalone/wsgi.py
+++ todo/standalone/wsgi.py
@@ -7,10 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
 
 application = get_wsgi_application()

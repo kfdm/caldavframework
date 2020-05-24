be6034dfe7377dd28158e2acb6e9b5d2ff4de41e
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-05-24 15:36:29 +0900

    Add signal for etag

---
+++ todo/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import pre_save
+from django.utils.crypto import get_random_string
+
+
+def set_etag(instance, **kwargs):
+    instance.etag = get_random_string(
+        length=instance._meta.get_field("etag").max_length
+    )
+
+
+pre_save.connect(set_etag, "todo.Event")
+pre_save.connect(set_etag, "todo.Calendar")

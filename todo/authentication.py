8b1613f5408eb25d873268b497314097edff52f5
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-04-22 20:42:05 +0900

    Remove debug output

--- todo/authentication.py
+++ todo/authentication.py
@@ -5,5 +5,4 @@ class BasicAuthentication(authentication.BasicAuthentication):
     def authenticate_credentials(self, userid, password, request=None):
         if "@" in userid:
             userid, domain = userid.split("@")
-            print(userid, domain)
         return super().authenticate_credentials(userid, password, request)

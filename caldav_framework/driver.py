31cc247e76304ddc2c30f3162d3cae6a61795ff7
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-05-04 12:46:39 +0900

    First pass

--- caldav_framework/driver.py
+++ caldav_framework/driver.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
 import logging
 import xml.etree.ElementTree as ET
+
+from django.utils.functional import cached_property
 
 from .response import MultistatusResponse
 
@@ -13,9 +16,39 @@ ET.register_namespace("DAV:", "")
 logger = logging.getLogger(__name__)
 
 
+def dispatch(method, prop):
+    def inner(func):
+        func.method = method
+        func.prop = prop
+        return func
+
+    return inner
+
+
 class BaseCollection:
     def __init__(self, obj):
         self.obj = obj
+
+    @cached_property
+    def handlers(self):
+        funcs = defaultdict(dict)
+        for obj in dir(self):
+            try:
+                method = getattr(self, "method", None)
+                prop = getattr(self, "prop", None)
+                funcs[method][prop] = obj
+            except KeyError:
+                pass
+        return funcs
+
+    def dispatch(self, method, prop, **kwargs):
+        ele = ET.Element(prop.text)
+        try:
+            func = self.handlers[method][prop.tag]
+        except KeyError:
+            return 404, ele
+        else:
+            return func(self, ele=ele, prop=prop, **kwargs)
 
     def report(self, request: HttpRequest, response: MultistatusResponse, href: str):
         query = request.data.find("{DAV:}prop")
@@ -24,199 +57,178 @@ class BaseCollection:
             parts = resolve(href.text).kwargs
 
             for prop in query.getchildren():
-                status, value = self._report(request, prop.tag, prop.text, parts)
-                propstats[status].append(value)
+                status, result = self.dispatch(
+                    "report", request=request, prop=prop, parts=parts
+                )
+                propstats[status].append(result)
             propstats.render(request)
 
     def propfind(self, request: HttpRequest, response: MultistatusResponse, href: str):
         propstats = response.propstat(href)
         for prop in request.data.find("{DAV:}prop").getchildren():
-            status, value = self._propfind(request, prop.tag, prop.text)
-            propstats[status].append(value)
+            status, result = self.dispatch("propfind", request=request, prop=prop)
+            propstats[status].append(result)
         propstats.render(request)
         return propstats
 
     def proppatch(self, request: HttpRequest, response: MultistatusResponse, href: str):
         propstats = response.propstat(href)
         for prop in request.data.find("{DAV:}set").find("{DAV:}prop").getchildren():
-            status, result = self._proppatch(request, prop.tag, prop.text)
+            status, result = self.dispatch("propfind", request=request, prop=prop)
             propstats[status].append(result)
         propstats.render(request)
         return propstats
 
-    def _proppatch(self, request, prop, value):
-        ele = ET.Element(prop)
-        logger.warning("Not implemented %s for %s", prop, self.obj)
-        return 404, ele
-
-    def _propfind(self, request, prop, value):
-        ele = ET.Element(prop)
-        logger.warning("Not implemented %s for %s", prop, self.obj)
-        return 404, ele
-
-    def _report(self, request, prop, value, extras):
-        ele = ET.Element(prop)
-        logger.warning("Not implemented %s for %s", prop, self.obj)
-        return 404, ele
-
 
 class RootCollection(BaseCollection):
-    def _propfind(self, request, prop, value):
-        ele = ET.Element(prop)
+    @dispatch("propfind", "{DAV:}current-user-principal")
+    def current_user_principal(self, ele, request, **kwargs):
+        ET.SubElement(ele, "{DAV:}href").text = resolve_url(
+            "principal", user=request.user.username
+        )
+        return 200, ele
 
-        if prop == "{DAV:}current-user-principal":
-            ET.SubElement(ele, "{DAV:}href").text = resolve_url(
-                "principal", user=request.user.username
-            )
-            return 200, ele
+    @dispatch("propfind", "{DAV:}current-user-privilege-set")
+    def current_user_privilege_set(self, ele, **kwargs):
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}read")
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}all")
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write")
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write-properties")
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write-content")
+        return 200, ele
 
-        if prop == "{DAV:}resourcetype":
-            ET.SubElement(ele, "{DAV:}collection")
-            return 200, ele
+    @dispatch("propfind", "{DAV:}owner")
+    def owner(self, ele, request, **kwargs):
+        ET.SubElement(ele, "{DAV:}href").text = resolve_url(
+            "principal", user=request.user.username
+        )
+        return 200, ele
 
-        if prop == "{DAV:}current-user-privilege-set":
-            ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}read")
-            ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}all")
-            ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write")
-            ET.SubElement(
-                ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write-properties"
-            )
-            ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write-content")
-            return 200, ele
+    @dispatch("propfind", "{DAV:}supported-report-set")
+    def supported_report_set(self, ele, **kwargs):
+        return 200, ele
 
-        if prop == "{DAV:}owner":
-            ET.SubElement(ele, "{DAV:}href").text = resolve_url(
-                "principal", user=request.user.username
-            )
-            return 200, ele
+    @dispatch("propfind", "{urn:ietf:params:xml:ns:caldav}calendar-user-address-set")
+    def calendar_user_address_set(self, ele, request, **kwargs):
+        ET.SubElement(ele, "{DAV:}href").text = resolve_url(
+            "principal",
+            user=request.user.username,
+        )
+        return 200, ele
 
-        if prop == "{DAV:}supported-report-set":
-            return 200, ele
+    @dispatch("propfind", "{urn:ietf:params:xml:ns:caldav}calendar-home-set")
+    def calendar_home_set(self, ele, request, **kwargs):
+        ET.SubElement(ele, "{DAV:}href").text = resolve_url(
+            "principal", user=request.user.username
+        )
+        return 200, ele
 
-        if prop == "{urn:ietf:params:xml:ns:caldav}calendar-user-address-set":
-            ET.SubElement(ele, "{DAV:}href").text = resolve_url(
-                "principal",
-                user=request.user.username,
-            )
-            return 200, ele
-
-        if prop == "{urn:ietf:params:xml:ns:caldav}calendar-home-set":
-            ET.SubElement(ele, "{DAV:}href").text = resolve_url(
-                "principal", user=request.user.username
-            )
-            return 200, ele
-
-        if prop == "{DAV:}resourcetype":
-            ET.SubElement(ele, "{DAV:}principal")
-            ET.SubElement(ele, "{DAV:}collection")
-            return 200, ele
-
-        logger.debug("unknown propfind %s for collection %s ", prop, self.obj)
-        return 404, ele
+    @dispatch("propfind", "{DAV:}resourcetype")
+    def resource_type(self, ele, **kwargs):
+        ET.SubElement(ele, "{DAV:}principal")
+        ET.SubElement(ele, "{DAV:}collection")
+        return 200, ele
 
 
 class Calendar(BaseCollection):
-    def _report(self, request, prop, value, extra):
-        ele = ET.Element(prop)
-        todo = self.obj.event_set.get(id=extra["task"])
+    @dispatch("report", "{DAV:}getetag")
+    def report_getetag(self, ele, **kwargs):
+        todo = self.obj.event_set.get(id=kwargs["task"])
+        ele.text = '"' + todo.etag + '"'
+        return 200, ele
 
-        if prop == "{DAV:}getetag":
-            ele.text = '"' + todo.etag + '"'
-            return 200, ele
+    @dispatch("report", "{urn:ietf:params:xml:ns:caldav}calendar-data")
+    def report_calendar_data(self, ele, **kwargs):
+        todo = self.obj.event_set.get(id=kwargs["task"])
+        ele.text = todo.to_ical()
+        return 200, ele
 
-        if prop == "{urn:ietf:params:xml:ns:caldav}calendar-data":
-            ele.text = todo.to_ical()
-            return 200, ele
+    @dispatch("propfind", "{DAV:}getetag")
+    def getetag(self, ele, **kwargs):
+        ele.text = '"' + self.obj.etag + '"'
+        return 200, ele
 
-        return 404, ele
+    @dispatch("propfind", "{DAV:}displayname")
+    def displayname(self, ele, **kwargs):
+        ele.text = self.obj.name
+        return 200, ele
 
-    def _propfind(self, request, prop, value):
-        ele = ET.Element(prop)
+    @dispatch("propfind", "{DAV:}current-user-privilege-set")
+    def current_user_privilege_set(self, ele, **kwargs):
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}read")
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}all")
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write")
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write-properties")
+        ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write-content")
+        return 200, ele
 
-        if prop == "{DAV:}getetag":
-            ele.text = '"' + self.obj.etag + '"'
-            return 200, ele
+    @dispatch("propfind", "{DAV:}owner")
+    def owner(self, ele, **kwargs):
+        ET.SubElement(ele, "{DAV:}href").text = resolve_url(
+            "principal", user=self.obj.owner.username
+        )
+        return 200, ele
 
-        if prop == "{DAV:}displayname":
-            ele.text = self.obj.name
-            return 200, ele
+    @dispatch("propfind", "{DAV:}resourcetype")
+    def resourcetype(self, ele, **kwargs):
+        ET.SubElement(ele, "{urn:ietf:params:xml:ns:caldav}calendar")
+        ET.SubElement(ele, "collection")
+        return 200, ele
 
-        if prop == "{DAV:}current-user-privilege-set":
-            ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}read")
-            ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}all")
-            ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write")
-            ET.SubElement(
-                ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write-properties"
-            )
-            ET.SubElement(ET.SubElement(ele, "{DAV:}privilege"), "{DAV:}write-content")
-            return 200, ele
+    @dispatch(
+        "propfind", "{urn:ietf:params:xml:ns:caldav}supported-calendar-component-set"
+    )
+    def supported_calendar_compontnet_set(self, ele, **kwargs):
+        ET.SubElement(ele, "{urn:ietf:params:xml:ns:caldav}comp", {"name": "VTODO"})
+        return 200, ele
 
-        if prop == "{DAV:}owner":
-            ET.SubElement(ele, "{DAV:}href").text = resolve_url(
-                "principal", user=self.obj.owner.username
-            )
-            return 200, ele
+    @dispatch("propfind", "{urn:ietf:params:xml:ns:caldav}calendar-data")
+    def calendar_data(self, ele, **kwargs):
+        ele.text = self.obj.to_ical()
+        return 200, ele
 
-        if prop == "{DAV:}resourcetype":
-            ET.SubElement(ele, "{urn:ietf:params:xml:ns:caldav}calendar")
-            ET.SubElement(ele, "collection")
-            return 200, ele
+    @dispatch("propfind", "{DAV:}getcontenttype")
+    def getcontenttype(self, ele, **kwargs):
+        ele.text = "text/calendar"
+        return 200, ele
 
-        if prop == "{urn:ietf:params:xml:ns:caldav}supported-calendar-component-set":
-            ET.SubElement(ele, "{urn:ietf:params:xml:ns:caldav}comp", {"name": "VTODO"})
-            return 200, ele
+    @dispatch("propfind", "{DAV:}supported-report-set")
+    def supported_report_set(self, ele, **kwargs):
+        return 200, ele
 
-        if prop == "{urn:ietf:params:xml:ns:caldav}calendar-data":
-            ele.text = self.obj.to_ical()
-            return 200, ele
+    @dispatch("propfind", "{http://apple.com/ns/ical/}calendar-color")
+    def calendar_color(self, ele, **kwargs):
+        ele.text = self.obj.color
+        return 200, ele
 
-        if prop == "{DAV:}getcontenttype":
-            ele.text = "text/calendar"
-            return 200, ele
+    @dispatch("propfind", "{http://apple.com/ns/ical/}calendar-order")
+    def calendar_order(self, ele, **kwargs):
+        ele.text = str(self.obj.order)
+        return 200, ele
 
-        if prop == "{DAV:}supported-report-set":
-            return 200, ele
+    @dispatch("proppatch", "{DAV:}displayname")
+    def patch_displayname(self, ele, prop, **kwargs):
+        self.obj.name = prop.value
+        return 200, ele
 
-        if prop == "{http://apple.com/ns/ical/}calendar-color":
-            ele.text = self.obj.color
-            return 200, ele
+    @dispatch("proppatch", "{http://apple.com/ns/ical/}calendar-color")
+    def patch_calendar_color(self, ele, prop, **kwargs):
+        self.obj.color = prop.text
+        return 200, ele
 
-        if prop == "{http://apple.com/ns/ical/}calendar-order":
-            ele.text = str(self.obj.order)
-            return 200, ele
-
-        logger.debug("unknown propfind %s for calendar %s ", prop, self.obj)
-        return 404, ele
-
-    def _proppatch(self, request, prop, value):
-        ele = ET.Element(prop)
-
-        if prop == "{DAV:}displayname":
-            self.obj.name = value
-            return 200, ele
-
-        if prop == "{http://apple.com/ns/ical/}calendar-color":
-            self.obj.color = value
-            return 200, ele
-
-        if prop == "{http://apple.com/ns/ical/}calendar-order":
-            self.obj.order = value
-            return 200, ele
-
-        logger.debug("unknown proppatch %s for calendar %s ", prop, self.obj)
-        return 404, ele
+    @dispatch("proppatch", "{http://apple.com/ns/ical/}calendar-order")
+    def patch_calendar_order(self, ele, prop, **kwargs):
+        self.obj.order = prop.text
+        return 200, ele
 
 
 class Task(BaseCollection):
-    def _propfind(self, request, prop, value):
-        ele = ET.Element(prop)
+    @dispatch("propfind", "{DAV:}getetag")
+    def getetag(self, ele, **kwargs):
+        ele.text = '"' + self.obj.etag + '"'
+        return 200, ele
 
-        if prop == "{DAV:}getetag":
-            ele.text = '"' + self.obj.etag + '"'
-            return 200, ele
-
-        if prop == "{DAV:}getcontenttype":
-            ele.text = "text/calendar;charset=utf-8;component=VTODO"
-            return 200, ele
-
-        return 404, ele
+    @dispatch("propfind", "{DAV:}getcontenttype")
+    def getcontenttype(self, ele, **kwargs):
+        ele.text = "text/calendar;charset=utf-8;component=VTODO"
+        return 200, ele
